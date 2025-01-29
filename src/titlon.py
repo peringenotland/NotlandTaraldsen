1ac31c4d6c32293eb72c561d0f6b874df4fba315
peringenotland <peringenot@gmail.com>
2025-01-29 13:11:23 +0100

    Vital update, fixed bug data non existing directory

--- src/titlon.py
+++ src/titlon.py
@@ -4,13 +4,13 @@ import pandas as pd
 import pymysql
 
 # Load enviroment variables from .env file
-# .env contains username and password: USERNAME=xxxxxx@ntnu.no and PASSWORD=xxxxxxxxxxxxxxxxx
+# .env contains username and password: TITLON_USERNAME=xxxxxx@ntnu.no and TITLON_PASSWORD=xxxxxxxxxxxxxxxxx
 load_dotenv()
 
 #Query script for MySQL client
 con = pymysql.connect(host='titlon.uit.no', 
-                    user=os.getenv('USERNAME'), 
-                    password=os.getenv('PASSWORD'), 
+                    user=os.getenv('TITLON_USERNAME'), 
+                    password=os.getenv('TITLON_PASSWORD'), 
                     database='OSE')  
 crsr=con.cursor()
 crsr.execute("SET SESSION MAX_EXECUTION_TIME=60000;")
@@ -23,9 +23,7 @@ r=crsr.fetchall()
 df=pd.DataFrame(list(r), columns=[i[0] for i in crsr.description])
 print(df)
 
-df.to_csv('./data/titlon_accounting.csv', index=False)
-
-
+df.to_csv('titlon_accounting.csv', index=False)
 
 
 #YOU NEED TO BE CONNECTED TO YOUR INSTITUTION VIA VPN, OR BE AT THE INSTITUTION, FOR THIS CODE TO WORK
