07ec9c81b8f7601fbef27e47572fd8b1e4150791
David Taraldsen <taraldsendavid@gmail.com>
2025-01-30 12:00:51 +0100

    added neldata file and scatecdatafile that gets data from csv

--- src/titlon.py
+++ src/titlon.py
@@ -16,16 +16,29 @@ con = pymysql.connect(host='titlon.uit.no',
                     password=os.getenv('TITLON_PASSWORD'), 
                     database='OSE')  
 crsr=con.cursor()
-crsr.execute("SET SESSION MAX_EXECUTION_TIME=60000;")
+crsr.execute("SET SESSION MAX_EXECUTION_TIME=600000;")
 crsr.execute("""
-	SELECT  * FROM `OSE`.`account`
-	ORDER BY `Name`,`Year`
+SELECT 
+             `Name`, `Year`, `description`, `Value`, `ID` 
+FROM `OSE`.`account` 
+WHERE `companyID` = 7795
+AND `Year` <= 2022
+ORDER BY `Name`, `Year`;
 """)
 r=crsr.fetchall()
 df=pd.DataFrame(list(r), columns=[i[0] for i in crsr.description])
 print(df)
 
-df.to_csv('titlon_accounting.csv', index=False)
+#df.to_csv('titlon_accounting.csv', index=False)
 
 
 #YOU NEED TO BE CONNECTED TO YOUR INSTITUTION VIA VPN, OR BE AT THE INSTITUTION, FOR THIS CODE TO WORK
+
+file_path = "C:\Users\taral\Documents\Dev\Master\NotlandTaraldsen\titlon_accounting.csv"  # Change this to your actual file path
+df = pd.read_csv(file_path)
+
+# Filter for the specific company and years up to 2022
+company_id = 7795  # Replace with your desired company ID
+filtered_df = df[(df['companyID'] == company_id) & (df['Year'] <= 2022)]'
+revenue_df = filtered_df[filtered_df['description'].str.contains("revenue", case=False, na=False)]
+print(revenue_df[['Name', 'Year', 'description', 'Value', 'ID']])
