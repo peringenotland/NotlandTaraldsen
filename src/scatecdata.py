0e211f9bcc9ecd2a1434fc375243440368d96cc9
Per Inge Notland <peringenot@gmail.com>
2025-03-19 14:02:21 +0100

    Ryddet i filene etter Titlon

--- src/scatecdata.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-file_path = "C:\\Users\\taral\\Documents\\Dev\\Master\\NotlandTaraldsen\\compustat_scatec.csv"  # Change this to your actual file path
-df = pd.read_csv(file_path)
-
-print(df[['revtq','cogsq', 'xoprq']])
